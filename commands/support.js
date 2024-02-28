b36c7872524ab8d14b7037efd1725da0a5c7785a
YJ <104505841+tosilan@users.noreply.github.com>
2024-02-28 20:03:24 +0900

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -18,7 +18,7 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "support",
-  description: "Get support server link",
+  description: "サポートサーバーのリンクを取得する",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
@@ -28,7 +28,7 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setColor('#FFFFFF')
             .setTitle('Support server')
-            .setDescription(`Click [here](${supportServerLink}) to join our server.\nWe will be there for you anytime ❤️`)
+            .setDescription(`[参加]( ${supportServerLink} )をクリックしてサーバーに参加してください。\n私たちはいつでもあなたのためにそこにいます❤️`)
             
             .setTimestamp();
 
