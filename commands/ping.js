3ebf5fd1e8198cf4b458179080db8cc2ec558582
YJ <104505841+tosilan@users.noreply.github.com>
2024-02-28 19:49:11 +0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -17,7 +17,7 @@ const { EmbedBuilder } = require('discord.js')
 
 module.exports = {
   name: "ping",
-  description: "check the bot latency",
+  description: "ボットのpingを確認する",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
